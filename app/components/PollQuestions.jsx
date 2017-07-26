5bae641665049863811e7031f3c1c9c53877d8dc
Rachel Berkowitz <brkwtzr@gmail.com>
2017-07-26 13:58:33 -0400

    randomized question order-#6

--- app/components/PollQuestions.jsx
+++ app/components/PollQuestions.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import _ from 'lodash'
 
 import QuestionBox from './QuestionBox.jsx'
 
 export default class PollQuestions extends React.Component {
   render() {
+    const randomizedQuestions = _.shuffle(this.props.questions)
+
     return (
       <div id="questions">
         <h2>Do you agree with the following statements:</h2>
-        {this.props.questions.map(question => <QuestionBox key={question.prompt} question={question}/>)}
+        {randomizedQuestions.map(question => <QuestionBox key={question.prompt} question={question}/>)}
       </div>
     )
   }
