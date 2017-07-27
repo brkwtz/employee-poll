569a5d8017b4137b0c2aba447340cef45619ef01
Rachel Berkowitz <brkwtzr@gmail.com>
2017-07-27 11:23:07 -0400

    running on my own server instead of webpack dev server

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express')
 const app = express()
 const path = require('path')
@@ -6,8 +8,9 @@ const morgan = require('morgan')
 
 //middleware
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, './build')))
 
 //listen
 const port = process.env.PORT || 3000
@@ -15,6 +18,10 @@ const server = app.listen(port, () => {
   console.log('listening on port', port)
 })
 
-app.get('', (req, res, next) => {
+app.get('/', (req, res, next) => {
   res.sendFile('index')
+})
+
+app.get('/:rating', (req, res, next) => {
+  res.json(req.rating)
 })
